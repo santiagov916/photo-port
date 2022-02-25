fda2fe04c3db6d24c1080ccb0cf85c6f54245447
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-02-24 16:40:29 -0800

    tested About component; passed

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, cleanup  } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+
+describe('About component', () => {
+    it('renders', () => {
+        render(<About />);
+    });
+
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+    
+    
+})
+
+afterEach(cleanup);
