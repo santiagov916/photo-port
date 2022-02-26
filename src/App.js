ab006b6fdbcd1813448928da92541bcfc2deec89
Santiago Venegas <santiagovenegas@Santiagos-MacBook-Pro.local>
2022-02-26 05:31:08 -0800

    reset back to navbar completion

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import React, { useState } from 'react';
-import Nav from './components/Nav';
+import React from 'react';
 import About from './components/About';
-import Gallery from './components/Gallery';
+import Nav from './components/Nav';
 
 function App() {
-
-  const [categories] = useState([
-    {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
-    },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]);
-
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
   return (
   <div>
-    <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}></Nav>
+    <Nav></Nav>
     <main>
-      <Gallery></Gallery>
       <About></About>
     </main>
 
